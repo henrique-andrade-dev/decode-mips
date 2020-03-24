797cafad1c4aef02b0fe5dee8eb80a7c333de34f
Henrique Andrade <henrique.andrade22@hotmail.com>
2020-03-24 20:04:20 -0300

    New class HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,45 @@
 import java.io.File; // Import the File class
 import java.io.FileNotFoundException; // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.*; // 
 
 class Main {
     public static void main(String[] args) {
         try {
-            File myObj = new File("filename.txt");
+            File myObj = new File("./assets/input.txt");
             Scanner myReader = new Scanner(myObj);
+            HashMap<String, String> Opcodes = new HashMap<>();
+            String[] opcodes = {"add", "addi", "sub", "mult", "div", "neg", "and", "andi", "or", "ori", "xor", "nor", "slt", "slti", "sll", "srl", "lw", "sw", "beq", "bne", "j", "jr", "jal", "nop"};
+            String binary = "";
 
             while (myReader.hasNextLine()) {
+                
+                
                 String data = myReader.nextLine();
-                System.out.println(data);
+                String[] parts = data.split("jgodijfgoi");
+                
+                switch (parts[0]) {
+                    case "add": binary += "00000";
+                        
+                        break;
+
+                        case "addi": binary += "00000";
+                        
+                        break;
+                        
+                
+                    default:
+                        break;
+                }
+
+
+                for (int i = 0; i < parts.length; i++) {
+                }
+
+
+                //String bin = Integer.toBinaryString(dec);
+
+                System.out.println();
             }
             
             myReader.close();
